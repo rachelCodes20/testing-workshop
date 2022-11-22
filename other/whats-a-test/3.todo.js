38e067cfdcf60b6023d415c3d2886cca900a07a9
Rachel Collins <spiritedndiligent@gmail.com>
2022-11-21 22:54:19 -0500

    adding some basic tests

--- other/whats-a-test/3.todo.js
+++ other/whats-a-test/3.todo.js
@@ -27,15 +27,25 @@ Then run this code with `node 3.todo`
 
 const {sum, subtract} = require('./math')
 
-let result, expected
+test('sum adds numbers', () => {
+  const result = sum(3,7)
+  const expected = 10;
+  expect(result).toBe(expected);
+})
 
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
+test('sum subtracts numbers', () => {
+  const result = subtract(7, 3)
+  const expected = 4
+  expect(result).toBe(expected)
+})
 
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
+function test(title, callback){
+  try{
+    callback()
+  }catch (error) {
+    console.log(error)
+  }
+}
 
 function expect(actual) {
   return {
